fc5ad13be1afb714e3bfb0b2a8f335612a16486b
hilo <hilo@hilonoMacBook-Air.local>
2017-05-30 02:01:10 +0900

    DDL追加

---
+++ sql/master_data.sql
@@ -0,0 +1,23 @@
+INSERT INTO unit (id, name) values (1, 'μ’ｓ');
+INSERT INTO unit (id, name) values (2, 'bibi');
+INSERT INTO unit (id, name) values (3, 'lily white');
+INSERT INTO unit (id, name) values (4, 'pritemps');
+
+
+INSERT INTO song (id, name) values (1, 'Angelic Angel');
+INSERT INTO song (id, name) values (2, 'COLORFUL VOICE');
+INSERT INTO song (id, name) values (3, 'cutie panther');
+INSERT INTO song (id, name) values (4, 'PSYCHIC FIRE');
+INSERT INTO song (id, name) values (5, '微熱からMystery');
+INSERT INTO song (id, name) values (6, '知らないLove*教えてLove');
+INSERT INTO song (id, name) values (7, 'Pure girls project');
+INSERT INTO song (id, name) values (8, '小夜啼鳥恋詩');
+
+INSERT INTO unit_song (id, unit_id, song_id) values (1, 1, 1);
+INSERT INTO unit_song (id, unit_id, song_id) values (2, 1, 2);
+INSERT INTO unit_song (id, unit_id, song_id) values (3, 2, 3);
+INSERT INTO unit_song (id, unit_id, song_id) values (4, 2, 4);
+INSERT INTO unit_song (id, unit_id, song_id) values (5, 3, 5);
+INSERT INTO unit_song (id, unit_id, song_id) values (6, 3, 6);
+INSERT INTO unit_song (id, unit_id, song_id) values (7, 4, 7);
+INSERT INTO unit_song (id, unit_id, song_id) values (8, 4, 8);
